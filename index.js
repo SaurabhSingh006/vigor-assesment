3998b8b0db6f83523b020f571be5db6d96cd8935
Ubuntu <saurabh.singh@vnnogile.in>
2023-01-28 16:47:54 +0530

    Set up express

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: true,
+        message: "success"
+    })
+});
+
 app.get('/api/v1/heartbeat', (req, res) => {
     res.status(200).json({
         status: true,
